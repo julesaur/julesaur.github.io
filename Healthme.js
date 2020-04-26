8f82f83df543d57e4b21756df224a1bb74121e18
julesaur <julee.nhek@sjsu.edu>
2020-04-26 16:45:59 -0700

    update code

--- Healthme.js
+++ Healthme.js
@@ -24,10 +24,10 @@ function draw() {
   if (gameState=="L1"){
   levelOne();
   } 
-   
+
   if (gameState=="L2"){
    levelTwo(); 
-    fill(2, 34, 93);
+
   }
   if (gameState=="L3"){
    levelThree();  
@@ -59,7 +59,7 @@ fill(255);
     bally= random(height);
     score= score +1;
   }
-  if(score>15){
+  if(score>14){
 // call level 2
  // fill(random(255));
  gameState= "L2";
@@ -71,9 +71,9 @@ fill(255);
 } // end level one
 
 function levelTwo(){
-  background(237, 34, 93);
    for (var y = 0;   y <=  height;  y +=  40) {    
  for (var x = 0;   x <=  width   ;  x +=  40) {
+     fill(255,186,186);
      rect(x ,  y, 40, 40);
  }}
     
@@ -85,13 +85,13 @@ fill(255);
     bally= random(height);
     score= score +1;
   }
-  if(score>25){
+  if(score>29){
 // level 3
    gameState = "L3";
 
   }
   
-  fill(237, 34, 93);
+  fill(255,186,186);
   rect(ballx, bally, ballSize, ballSize);
 } // end level two
 
@@ -112,7 +112,7 @@ function levelThree(){
     ballSize=ballSize -1;
     score= score +1;
   }
-  if(score>40){
+  if(score>44){
 // end of game
    gameState = "L4";
   }
@@ -129,18 +129,18 @@ function levelFour(){
  text("Game Over", width/2, 40);
 image(img1, 130, 135, 300, 300);
     //bubble speech//
-      fill(255);
+fill(255);
   stroke(1);
   rect(40, 105, 185, 105);
   fill(1);
-  rect(42, 109, 10, 10);
-  rect(35, 120, 10, 90);
+  rect(40, 105, 10, 20);
+  rect(30, 120, 10, 90);
   rect(42, 209, 10, 10);
   rect(42, 209, 170, 10);
   rect(215, 202, 10, 10);
   rect(223, 117, 10, 85);
-  rect(50, 95, 160, 10);
-   rect(210, 109, 13, 10);
+  rect(50, 95, 170, 10);
+   rect(215, 95, 10, 30);
 
   //blackborders//
    rect(135, 209, 10, 40);
